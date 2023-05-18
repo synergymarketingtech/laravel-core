1d242f82c409cbb8256fb0ba99fe9fcb710ae65e
Dipak Sarkar <hello@dipaksarkar.in>
2023-05-18 10:52:53 +0530

    added gitignoe and update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -27,9 +26,6 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
-
-        // set auto increment to 100000
-        DB::statement('ALTER TABLE users AUTO_INCREMENT = 1000000;');
     }
 
     /**
