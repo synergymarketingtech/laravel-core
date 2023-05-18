a9f85eb434bf435d9e4e14b34d19c0984d1f1c4a
Dipak Sarkar <hello@dipaksarkar.in>
2023-05-18 10:45:29 +0530

    updated

--- database/migrations/2014_10_12_000001_create_admins_table.php
+++ database/migrations/2014_10_12_000001_create_admins_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -27,6 +28,9 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        // set auto increment to 100000
+        DB::statement('ALTER TABLE users AUTO_INCREMENT = 1000000;');
     }
 
     /**
